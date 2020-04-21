a7d646e81506d1e1d39be5fa44c54cf5cb65b571
Jorge Cortés <jorgeacortes@users.noreply.github.com>
2020-04-21 17:17:11 +0200

    Updated to latest socket.io and express packages.

--- server.js
+++ server.js
@@ -1,34 +1,20 @@
-var express = require('express'),
-    http = require('http'),
-    path = require('path')
-    io = require('socket.io');
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-// Express configuration boilerplate
-var app = express();
-app.configure(function() {
-    app.set('port', process.env.PORT || 3000);
-    app.use(express.static(path.join(__dirname, 'public')));
+const  port = process.env.PORT || 3000;
+
+/* App */
+server.listen(port, () => {
+    console.log('Server listening at port %d', port);
 });
-app.configure('development', function() {
-    app.use(express.errorHandler());
-});
-
-
-// Set up express server
-var server = http.createServer(app).listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
-});
-
-// Set up socket.io
-var io = require('socket.io').listen(server);
-
-io.configure(function () { 
-    io.set("transports", ["xhr-polling"]); 
-    io.set("polling duration", 10); 
-});
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Handle socket traffic
-io.sockets.on('connection', function (socket) {
+io.on('connection', (socket) => {
     // Relay chat data to all clients
     socket.on('myInputUpdate', function(data) {
         socket.broadcast.emit('myInputUpdate', data);
